d1ae1d5677c7bcbe444f6ae083eee466f43e2d7a
Granto1 <grantoyt@gmail.com>
2026-01-31 20:42:37 -0500

    Some comments

--- API Work/calendar.py
+++ API Work/calendar.py
@@ -7,26 +7,29 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-# If modifying these scopes, delete the file token.json.
+# this assumes that the type of calendar is primary, we could test secondary but I don't think that's incredibly useful
+
+# if modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/calendar"]
 
-
-def createEvent(bodyJson):
+# Creates an event
+# bodyJson: the content of the event to insert
+def createEvent(bodyJson, service):
     # create event with parameters
     event = service.events().insert(calendarId='primary', body=bodyJson).execute()
-    # print('Event created: %s' % (event.get('htmlLink')))
-
 
 # @returns a list of calendar events
-# this is decided by the a starting time and the number of results desired
-def readEvent(timeStart=datetime.datetime.now(tz=datetime.timezone.utc).isoformat(), results=10):
+# timeStart: the date to start parsing
+# numResults: the number of results to return
+# service: the service variable for the calendar
+def readEvent(timeStart, numResults, service):
     # For read event, we will use the number of events to return and the start/end
     events_result = (
         service.events()
         .list(
             calendarId="primary",
             timeMin=timeStart,
-            maxResults=results,
+            maxResults=numResults,
             singleEvents=True,
             orderBy="startTime",
         )
@@ -34,7 +37,9 @@ def readEvent(timeStart=datetime.datetime.now(tz=datetime.timezone.utc).isoforma
     )
     return events_result
 
-    
+# Updates a specified event with the provided json
+# event_to_change: the event ID to change
+# event_update_body: the content of the new event in json form
 def updateEvent(event_to_change, event_update_body):
     # Call the events().patch() method
     updated_event = service.events().patch(
@@ -44,7 +49,8 @@ def updateEvent(event_to_change, event_update_body):
         sendUpdates='all'  # Optional: determines if guests receive notifications ('all', 'externalOnly', or 'none')
     ).execute()
 
-    
+# Deletes a specified event
+# event_to_delete: ID of the event to delete
 def deleteEvent(event_to_delete):
     # delete event
     service.events().delete(calendarId='primary', eventId=event_to_delete).execute()
